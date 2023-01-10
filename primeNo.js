abb2c3545207b72301eee692cf385597507554d4
Puja Manna <97797227+Puja1997@users.noreply.github.com>
2023-01-10 17:32:45 +0530

    code is fix by member2

---
+++ primeNo.js
@@ -0,0 +1,17 @@
+let a=13;
+let c=0;
+for(let i=2;i<a/2;i++)
+  {
+    if(a%2==0);
+    {
+      c++;
+    }
+  }
+
+if(c==2);
+{
+  console.log("Prime Number");
+}
+else{
+  console.log("Not a prime number");
+}
